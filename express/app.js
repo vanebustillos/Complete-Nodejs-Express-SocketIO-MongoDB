8fef84b4cd3f8aaaef9a6363465164c437651142
vanebustillos <vanebustillos1@gmail.com>
2022-07-12 16:28:35 -0400

    Adding ejs functionalities

--- express/app.js
+++ express/app.js
@@ -98,19 +98,29 @@ readStream.on('data', chunk => {
 //readStream.pipe(writeStream) 
 
 const express = require('express')
-const app = express()
+const app = express();
+app.set('view engine', 'ejs');
+app.listen(3000);
 
 app.get('/', (req, res) => {
     //res.send('<p>Home Page</p>')
-    res.sendFile('./views/index.html', {root: __dirname})
+    //res.sendFile('./views/index.html', {root: __dirname})
+    //res.render('index', { title2: 'some titlee2'});
+    const items = [
+        { name: 'mobile phone', price: 1000 },
+        { name: 'book', price: 30 },
+        { name: 'computer', price: 2000 },
+    ]
+    res.render('index', {items});
 });
 
 app.get('/add-item', (req, res) => {
-    res.sendFile('./views/add-item.html', {root: __dirname})
+   // res.sendFile('./views/add-item.html', {root: __dirname})
+   res.render('add-item')
 });
 
 app.use((req,res) => {
-    res.sendFile('./views/error.html', {root: __dirname})
+    //res.sendFile('./views/error.html', {root: __dirname})
+    res.render('error')
 }); //usarlo siempre al final
 
-app.listen(3000);
